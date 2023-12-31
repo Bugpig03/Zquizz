377b4a74e9ab97f0b0b895360e7f17b6f107ffd6
bugpig <bugpigx@gmail.com>
2023-12-31 04:21:17 +0100

    Random on answer locations
    add random locations for answers

--- control.gd
+++ control.gd
@@ -1,72 +1,66 @@
 extends Control
 
-#@onready var DisplayText = $DisplayText
-#@onready var ListItem = $ItemList
-#@onready var RestardButton = $RestardBtn
+# Variables declarations (Process)
+var rng_with_seed = RandomNumberGenerator.new()
+var quiz_data = " "
+var correctAnswer = 0
 
-var rng = RandomNumberGenerator.new()
-var seed = 0
-var quizData = " "
-var randomGeneratedIndex = 0
+# Variables for game
+var custom_seed = 0
 var score = 0
+var max_question = 20
+var current_question = 0
 
-#VARIABLES QUIZ
+# Variables for data
 var question = " "
+var answers = []
 var answer1 = " "
 var answer2 = " "
 var answer3 = " "
 var answer4 = " "
-var correctAnswer = 0
 
 
+func init():
+	get_data_from_json("res://data/quiz_data.json") # get data from json
+	randomize() # randomize number for random
+	define_seed(randi_range(0, 999999999999)) # set seed for the game
+
+func define_seed(int_seed):
+	custom_seed = int_seed
+	$TextEdit.text = str(custom_seed)
+	rng_with_seed.seed = custom_seed
+	
 func _ready():
-	get_data_from_json("res://data/quiz_data.json")
-	randomize()
-	seed = randi_range(0, 999999999999)
-	$TextEdit.text = str(seed)
-	rng.seed = seed
-	#reset_color_answer()
+	init()
+	#random_answer()
 	
 func _process(delta):
 	pass
 	
 func _on_restard_btn_pressed():
-	randomGeneratedIndex = rng.randi_range(1, 543)
-	pick_question(randomGeneratedIndex)
+	pick_question(rng_with_seed.randi_range(1, 543))
 	reset_color_answer()
+	random_answer()
 	refresh_ui()	
 
-func _on_init_button_pressed():
-	seed = int($TextEdit.text)
-	rng.seed = seed
-	
+
+
 func get_data_from_json(json_file_path):
 	var file = FileAccess.open(json_file_path, FileAccess.READ)
 	var content = file.get_as_text()
 	var json = JSON.new()
-	quizData = json.parse_string(content)
+	quiz_data = json.parse_string(content)
 
 func pick_question(random_index):
-	if (quizData.size() >= random_index):
-		question = quizData[random_index].get("Question")
-		answer1 = quizData[random_index].get("Reponse1")
-		answer2 = quizData[random_index].get("Reponse2")
-		answer3 = quizData[random_index].get("Reponse3")
-		answer4 = quizData[random_index].get("Reponse4")
-		correctAnswer = quizData[random_index].get("BonneReponse")
+	if (quiz_data.size() >= random_index):
+		question = quiz_data[random_index].get("question")
+		answers = [quiz_data[random_index].get("right_answer"), quiz_data[random_index].get("fake_answer1"), quiz_data[random_index].get("fake_answer2"), quiz_data[random_index].get("fake_answer3")]
 		
 		print("get data from index:", random_index)
 	else:
 		print("Data at index", random_index, "not found")
 		return null
 		
-func refresh_ui():
-	$DisplayText.text = str(question)
-	$BoxContainer/VBoxContainer/btnAnswer1.text = str(answer1)
-	$BoxContainer/VBoxContainer2/btnAnswer2.text = str(answer2)
-	$BoxContainer/VBoxContainer/btnAnswer3.text = str(answer3)
-	$BoxContainer/VBoxContainer2/btnAnswer4.text = str(answer4)
-	
 func display_correct_answer(greenButton):
 	$BoxContainer/VBoxContainer/btnAnswer1.add_theme_color_override("font_color", Color("FF0000"))
 	$BoxContainer/VBoxContainer2/btnAnswer2.add_theme_color_override("font_color", Color("FF0000"))
@@ -87,8 +81,7 @@ func reset_color_answer():
 	$BoxContainer/VBoxContainer2/btnAnswer2.add_theme_color_override("font_color", Color("FFFFFF"))
 	$BoxContainer/VBoxContainer/btnAnswer3.add_theme_color_override("font_color", Color("FFFFFF"))
 	$BoxContainer/VBoxContainer2/btnAnswer4.add_theme_color_override("font_color", Color("FFFFFF"))	
-	
-	
+		
 func _on_btn_answer_1_pressed():
 	answer_is_correct(1)
 	display_correct_answer(correctAnswer)
@@ -121,3 +114,25 @@ func answer_is_correct(player_answer):
 		print("fail")
 		$AudioStreamPlayer_sound_effect.stream = load("res://audio/sound/SFX_Fail.wav")
 	$AudioStreamPlayer_sound_effect.play()
+
+
+func _on_btn_new_game_pressed():
+	define_seed(int($TextEdit.text))
+	
+
+func random_answer():
+	var save_right_answer = answers[0]
+	print(save_right_answer)
+	randomize()
+	answers.shuffle()
+	for i in range(4):
+		if (str(save_right_answer) == str(answers[i])):
+			print(answers[i])
+			correctAnswer = i+1
+	
+func refresh_ui():
+	$DisplayText.text = str(question)
+	$BoxContainer/VBoxContainer/btnAnswer1.text = str(answers[0])
+	$BoxContainer/VBoxContainer2/btnAnswer2.text = str(answers[1])
+	$BoxContainer/VBoxContainer/btnAnswer3.text = str(answers[2])
+	$BoxContainer/VBoxContainer2/btnAnswer4.text = str(answers[3])
